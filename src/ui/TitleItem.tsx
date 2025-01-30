d2e70dc99a03074ede5b2a9c643db20957b490fb
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-01-30 17:59:02 +0100

    Update TitleItem.tsx

--- src/ui/TitleItem.tsx
+++ src/ui/TitleItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TitleItem(props) {
+function TitleItem(props:any) {
   return (
     <div className='relative z-40 mx-auto mb-12 max-w-[800px] w-full pt-6 text-center md:mb-20 lg:pt-16 flex flex-col items-center'>
         <div className='absolute z-0 top-[45px] text-wrap w-full h-fit text-center font-extrabold text-[40px] leading-[1] md:text-[95px] transform -translate-y-8  bg-gradient-to-t to-primary/80 from-black/30 text-transparent bg-clip-text'>
